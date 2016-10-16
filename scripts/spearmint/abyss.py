d4471937e1aa7c8feee0d48f37d63a416578f403
Shaun Jackman <sjackman@gmail.com>
2016-10-16 15:35:56 -0700

    scripts/spearmint/abyss.py: Use make

--- scripts/spearmint/abyss.py
+++ scripts/spearmint/abyss.py
@@ -9,13 +9,9 @@ import subprocess
 # Run ABySS
 def main(job_id, params):
     k = params['k']
-    reads = "/Users/sjackman/work/hackseq/2016_project_2/scripts/manual/data/200k.fq"
-    output = "/Users/sjackman/work/hackseq/2016_project_2/scripts/manual/results/200k/k%d" % k
-    if not os.path.isdir(output):
-        os.makedirs(output)
-    subprocess.call(shlex.split("abyss-pe -C %s name=hsapiens k=%d in=%s" % (output, k, reads)))
+    subprocess.call(shlex.split("make k=%d" % k))
     max_n50 = 0
-    with open("%s/hsapiens-stats.tab" % output) as csvfile:
+    with open("results/200k/k%d/hsapiens-scaffolds.fac.tsv" % k) as csvfile:
         csvfile.readline()
         csvreader = csv.reader(csvfile, delimiter="\t")
         for row in csvreader:
