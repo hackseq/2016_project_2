21a48f4fc8d0dfe73a25778c05427a04db9292dc
Shaun Jackman <sjackman@gmail.com>
2016-10-17 09:45:30 -0700

    spearmint: Add the parameter l

--- scripts/spearmint/abyss.py
+++ scripts/spearmint/abyss.py
@@ -9,16 +9,17 @@ import subprocess
 # Run ABySS
 def main(job_id, params):
     k = params['k']
-    subprocess.call(shlex.split("make k=%d" % k))
+    l = params['l']
+    subprocess.call(shlex.split("make k=%d l=%d" % (k, l)))
     max_n50 = 0
-    with open("results/200k/k%d/hsapiens-scaffolds.fac.tsv" % k) as csvfile:
+    with open("results/200k/k%d/l%d/hsapiens-scaffolds.fac.tsv" % (k, l)) as csvfile:
         csvfile.readline()
         csvreader = csv.reader(csvfile, delimiter="\t")
         for row in csvreader:
             n50 = int(row[5])
             max_n50 = max(max_n50, n50)
-    print "k=%d\tN50=%d" % (k, n50)
+    print "k=%d\tl=%d\tN50=%d" % (k, l, n50)
     return -max_n50
 
 if __name__ == "__main__":
-    main(None, {"k": 40})
+    main(None, {"k": 40, "l": 40})
