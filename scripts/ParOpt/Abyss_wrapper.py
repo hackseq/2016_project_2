53d214d783f1d21b11f4ebc9c1d2ceef59086022
Craig A. Glastonbury <craig.glastonbury@kcl.ac.uk>
2016-10-16 17:46:34 -0700

    two variable optimization (k,s)

--- scripts/ParOpt/Abyss_wrapper.py
+++ scripts/ParOpt/Abyss_wrapper.py
@@ -7,16 +7,18 @@ import scipy
 import os
 
 k = round(float(sys.argv[1]))
-k=int(k)
+s_var = round(float(sys.argv[2]))
 
+k=int(k)
+s_var=int(s_var)
 # parse abyss output so it just reports N50 - For each value of k - supplied by ParOpt
-def parse (k):
-	os.system('mkdir k%s' %k)
-	os.system('ln -s ../200k.fq.gz k%s/' %k)
-	os.system('abyss-pe -j 6 -C k%s name=reads k=%s v=-v in="200k.fq.gz" contigs 2>&1 | tee abyss.log' %(k,k))
+def parse (k,s_var):
+	os.system('mkdir k%s_%s' %(k,s_var))
+	os.system('ln -s ../200k.fq.gz k%s_%s/' %(k,s_var))
+	os.system('abyss-pe -j 6 -C k%s_%s name=reads k=%s s=%s v=-v in="200k.fq.gz" contigs 2>&1 | tee abyss.log' %(k,s_var,k,s_var))
 	test = int(subprocess.check_output("tac abyss.log | sed -n 3p | cut -f 6",shell=True).split("\n")[0])
 	val = np.array(float(test))
 	print("fx = {:.10e}".format(float(val)))
 	return "fx = {:.10e}".format(float(val))
 
-parse(int(k))
+parse(int(k),int(s_var))
