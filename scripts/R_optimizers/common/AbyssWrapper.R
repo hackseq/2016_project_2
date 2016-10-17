493b1faeb1805d85e4406ebda634312eb49c303b
jasgrewal <grewalj23@gmail.com>
2016-10-16 20:13:44 -0700

    Sometimes i forget how ifelse works...

--- scripts/R_optimizers/common/AbyssWrapper.R
+++ scripts/R_optimizers/common/AbyssWrapper.R
@@ -79,7 +79,7 @@ runAbyss200k <- function(k) {
 Abyss_n50 <- function(paramlist, infile="$PWD/data/200k.fq", outpref="200k"){
 	#paramlist=c(k,s)
 	k = paramlist[1]
-	ifelse(length(paramlist)==1, s=1000, s=paramlist[2])
+	s = felse(length(paramlist)==1, 1000, paramlist[2])
 	#Launch Abyss with input params
 	stats <- runAbyss(input=infile, name=outpref, k=k, s=s)
 	if (is.null(stats)){
@@ -92,7 +92,7 @@ Abyss_n50 <- function(paramlist, infile="$PWD/data/200k.fq", outpref="200k"){
 Abyss_n50_l50 <- function(paramlist, infile="$PWD/data/200k.fq", outpref="200k"){
 	#paramlist=c(k,s)
         k = paramlist[1]
-        ifelse(length(paramlist)==1, s=1000, s=paramlist[2])
+        s = ifelse(length(paramlist)==1, 1000, paramlist[2])
         #Launch Abyss with input params
 	stats <- runAbyss(input=infile, name=outpref, k=k, s=s)
 	if (is.null(stats)){return(-1)}
