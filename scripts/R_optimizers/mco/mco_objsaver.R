813273e922f79f7f70749794b23ec49b78c0dd55
Jasleen Grewal <jkg21@sfu.ca>
2016-10-17 13:34:57 -0700

    update funcs

--- scripts/R_optimizers/mco/mco_objsaver.R
+++ scripts/R_optimizers/mco/mco_objsaver.R
@@ -12,6 +12,10 @@ params = json_data$variables
 getpoprange <- function(x){ifelse(x$type=="INT",(round((x$max-x$min)/2) - (round((x$max-x$min)/2) %% 4)), ((x$max-x$min) - ((x$max-x$min) %% 4)))}
 
 inparam=params$k
+inparam1 = params$k
+inparam2 = params$s
+inparam3 = params$l
+
 #1 input, 1 output
 k_n50_fit = nsga2(Abyss_n50,idim=1,odim=1,lower.bounds=c(inparam$min), upper.bounds=c(inparam$max),popsize=getpoprange(inparam), generations=100,  mprob=0.01, cprob=0.2)
 #SAVE OBJ
@@ -23,10 +27,7 @@ k_n50_l50_fit = nsga2(Abyss_n50_l50,idim=1,odim=2,lower.bounds=c(inparam$min), u
 save(k_n50_l50_fit, list=character(), file=paste(getwd(), "/scripts/R_optimizers/mco/interm_objs/","k_n50_l50_fit.obj",sep=""))
 
 ##Call GA algorithm (two param fit)
-inparam1 = params$k
-inparam2 = params$s
-
-#2 inputs, 1 output
+#2 inputs, 1-2 outputs
 ks_n50_fit = nsga2(Abyss_n50,idim=2,odim=1,lower.bounds=c(inparam1$min, inparam2$min), upper.bounds=c(inparam1$max, inparam2$max),popsize=max(getpoprange(inparam1), getpoprange(inparam2)), generations=100, mprob=0.01, cprob=0.2) 
 
 save(ks_n50_fit, list=character(), file=paste(getwd(), "/scripts/R_optimizers/mco/interm_objs/","ks_n50_fit.obj",sep=""))
@@ -35,4 +36,11 @@ ks_n50_l50_fit = nsga2(Abyss_n50_l50,idim=2,odim=2,lower.bounds=c(inparam1$min, 
 
 save(ks_n50_l50_fit, list=character(), file=paste(getwd(), "/scripts/R_optimizers/mco/interm_objs/","ks_n50_l50_fit.obj",sep=""))
 
+#3 inputs, 1-2 outputs
+ksl_n50_fit = nsga2(Abyss_n50,idim=3,odim=1,lower.bounds=c(inparam1$min, inparam2$min, inparam3$min), upper.bounds=c(inparam1$max, inparam2$max, inparam3$max),popsize=max(getpoprange(inparam1), getpoprange(inparam2), getpoprange(inparam3)), generations=100, mprob=0.01, cprob=0.2)
 
+save(ksl_n50_fit, list=character(), file=paste(getwd(), "/scripts/R_optimizers/mco/interm_objs/","ksl_n50_fit.obj",sep=""))
+ 
+ksl_n50_l50_fit = nsga2(Abyss_n50_l50,idim=3,odim=2,lower.bounds=c(inparam1$min, inparam2$min, inparam3$min), upper.bounds=c(inparam1$max, inparam2$max, inparam3$max),popsize=max(getpoprange(inparam1), getpoprange(inparam2), getpoprange(inparam3)), generations=100,mprob = 0.01,cprob=0.2)
+
+save(ksl_n50_l50_fit, list=character(), file=paste(getwd(), "/scripts/R_optimizers/mco/interm_objs/","ksl_n50_l50_fit.obj",sep=""))
